f3d940268314d23195f6db3335612a220b7a4399
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2016-08-08 01:11:13 -0400

    Initial Commit

---
+++ COMP123_S2016_Ckawakam_300821245_A7/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2016_Ckawakam_300821245_A7
+{/*
+  * Description:This is the Form to calculate to cost of the movies which come from dataserver
+  * Author: Chinatsu Kawakami
+  * Create Date: 8th August 2016
+  * Modified Date: 8th August 2016
+  * Version: 0.0.1 - Initial Commit
+  */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
